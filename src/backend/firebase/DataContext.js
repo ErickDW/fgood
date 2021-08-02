a423e492eb460e5477e55c63dc495f91ef7b2da1
Kenpo <kenpoerickson@gmail.com>
2021-08-02 02:02:04 -0500

    yes

--- src/backend/firebase/DataContext.js
+++ src/backend/firebase/DataContext.js
@@ -15,33 +15,29 @@ export const DataProvider = ({children}) =>{
 
 useEffect(() => {
    
-        if(auth.user !== null){
-            db.collection('cuenta')
-            .where('idUsuer', '==', auth.userID ).orderBy('fecha','desc')
-            .onSnapshot((QuerySnapshot) =>{
-                if(QuerySnapshot.empty){
-                   
-                }else{
-                    let f = [];
-                    let contador = 1;
-                    QuerySnapshot.forEach( element =>{
-                        let fete = element.data().fecha;
-                        if(fete === null){
-                            fete = '00/00/0000'
-                        }else{
-                            fete = fete.toDate();
-                        }
-                        f.push({...element.data(), fecha: new util().obtenerfecha(fete), idCuenta: element.id, idc: contador++});
-                        
-                        
-                    });
-                    setData(f);
-                    return f;
-                }
-            })
-        
+    if(auth.user !== null){
+        db.collection('cuenta')
+        .where('idUsuer', '==', auth.userID ).orderBy('fecha','desc')
+        .onSnapshot((QuerySnapshot) =>{
+            if(QuerySnapshot.empty){
+                
+            }else{
+                let f = [];
+                let contador = 1;
+                QuerySnapshot.forEach( element =>{
+                    let fete = element.data().fecha;
+                    if(fete === null){
+                        fete = '00/00/0000'
+                    }else{
+                        fete = fete.toDate();
+                    }
+                    f.push({...element.data(), fecha: new util().obtenerfecha(fete), idCuenta: element.id, idc: contador++});
+                });
+                setData(f);
+                return f;
+            }
+        })
     }
-    
 }, [auth.user,auth.userID, db])
 
     return (
